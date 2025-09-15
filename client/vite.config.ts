90e2fc292a1323e0243ca988f2bc3076c921240e
kriti <kritishrivastav17@gmail.com>
2025-09-15 06:51:09 +0530

    deployed backend endpoint

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // const env = loadEnv(mode, process.cwd());
 
   return {
