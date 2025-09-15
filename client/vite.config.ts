139663d1f30c606c464da0ba10ff13321565a5c5
kriti <kritishrivastav17@gmail.com>
2025-09-15 16:17:24 +0530

    backend api url issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  // const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [
@@ -15,7 +15,7 @@ export default defineConfig(() => {
       port: 3000,
       proxy: {
         '/api': {
-          target: 'http://54.79.12.50:8000',
+          target: env.VITE_BACKEND_URL || 'http://localhost:8000',
           changeOrigin: true,
         },
       },
