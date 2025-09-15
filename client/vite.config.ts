b059c1b839841a4da63d2391dff4def59ad38202
kriti <kritishrivastav17@gmail.com>
2025-09-15 06:48:17 +0530

    deployed backend endpoint

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       proxy: {
-        '/': {
+        '/api': {
           target: env.VITE_BACKEND_URL || 'http://54.79.12.50:8000',
           changeOrigin: true,
         },
